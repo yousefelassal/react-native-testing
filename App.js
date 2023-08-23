eacac3235acd25db6c72a82eca640a309a4fb3ee
Yousef <yousefsameh24@gmail.com>
2023-08-23 20:34:44 +0300

    handle image selection

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
@@ -9,14 +10,16 @@ import Button from './components/Button';
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       quality: 1,
     });
 
-    if(!result.cancelled) {
-      console.log(result);
+    if(!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You did not select an image.');
     }
@@ -25,7 +28,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer 
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
@@ -56,4 +62,3 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-
