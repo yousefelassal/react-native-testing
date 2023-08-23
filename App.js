625286afa6d69820e50953d91214783048d09d72
Yousef <yousefsameh24@gmail.com>
2023-08-23 16:55:56 +0300

    add pressable

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
 
 export default function App() {
@@ -16,6 +16,9 @@ export default function App() {
   return (
     <View className="flex-1 flex-col gap-2 p-6 bg-amber-200">
       <Text className="text-2xl text-left text-blue-600 mt-6 pt-6">Hello World!</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.text}>Press Me</Text>
+      </Pressable>
       <View className="flex-1 justify-center items-center">
         <Animated.View style={{ ...styles.box, width }} />
         <View className="flex-row w-fit border-2 px-3 py-2 mb-1 rounded-full shadow-md bg-blue-200 border-blue-600 active:scale-110 active:bg-blue-600 transition-transform">
@@ -33,6 +36,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#b58df1',
     borderRadius: 20,
     marginVertical: 64,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    elevation: 3,
+    backgroundColor: 'black',
+    borderRadius: 8,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
 
