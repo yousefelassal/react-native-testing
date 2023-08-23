2674f81857cced89f6d40ebfb8d198a25a1f3bea
Yousef <yousefsameh24@gmail.com>
2023-08-23 20:26:24 +0300

    handle launching image library

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import * as ImagePicker from 'expo-image-picker';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
@@ -8,6 +9,19 @@ import Button from './components/Button';
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if(!result.cancelled) {
+      console.log(result);
+    } else {
+      alert('You did not select an image.');
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
