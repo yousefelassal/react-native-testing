5f43ac9b55746712ca0c67f8d752cb30523ffb9d
Yousef <yousefsameh24@gmail.com>
2023-08-23 21:20:08 +0300

    handle picked emoji

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import IconButton from './components/IconButton';
 import CircleButton from './components/CircleButton';
 import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
@@ -57,6 +58,7 @@ export default function App() {
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
