7675e4978b7ee1075cc57739dadb66c20b42d7ec
Yousef <yousefsameh24@gmail.com>
2023-08-25 15:30:53 +0300

    add tap gesture

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,16 +1,41 @@
 import { View, Image } from 'react-native';
-import Animated from 'react-native-reanimated';
+import { TapGestureHandler } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  withSpring,
+} from 'react-native-reanimated';
 
 const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+  const scaleImage = useSharedValue(imageSize);
+
+  const onDoubleTap = useAnimatedGestureHandler({
+    onActive: () => {
+      if(scaleImage.value !== imageSize * 2){
+        scaleImage.value = scaleImage * 2;
+      }
+    }
+  })
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });  
+
   return (
     <View style={{ top: -350 }}>
-      <AnimatedImage
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-      />
+      <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+        <AnimatedImage
+          source={stickerSource}
+          resizeMode="contain"
+          style={{ width: imageSize, height: imageSize }}
+        />
+      </TapGestureHandler>
     </View>
   );
 }
