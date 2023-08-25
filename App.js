d60c94bf46734419fa61d83bc3c701e43e263348
Yousef <yousefsameh24@gmail.com>
2023-08-25 15:23:54 +0300

    add gesture handler view

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import CircleButton from './components/CircleButton';
 import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
@@ -52,7 +53,7 @@ export default function App() {
 
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer 
           placeholderImageSource={PlaceholderImage}
@@ -78,7 +79,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
